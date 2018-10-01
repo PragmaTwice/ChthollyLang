dc35c684220d7846e624a8ed8aee714d7c304b11
PragmaTwice <e.d.boy@aliyun.com>
2018-10-01 20:54:09 +0800

    [WIP] IRGenerator: adjust Opcode && add trival c/dtor, oper(=/==/!=/unsigned)

--- src/chtholly/irgenerator.hpp
+++ src/chtholly/irgenerator.hpp
@@ -19,50 +19,45 @@ namespace Chtholly
 			Drop,
 			End
 		};
-		enum class Lambda : unsigned
+		enum class Function : unsigned
 		{
-			Begin = 0x20,
-			End
+			Start = 0x20,
+			Call
 		};
 		enum class List : unsigned
 		{
 			Push = 0x30,
-			Call
+			Pop
 		};
 		enum class Control : unsigned
 		{
-			Jump = 0x40,
-			JumpIf,
-			Mark
+			Jump = 0x40,			// 1
+			JumpIf,					// 2
+			Mark					// 1
 		};
 		enum class Object : unsigned
 		{
-			Var = 0x50,
-			VarWithInit,
-			VarWithConstraint,
-			VarWithInitAndConstraint,
-			Const,
-			ConstWithInit,
-			ConstWithConstraint,
-			ConstWithInitAndConstraint,
-			Use
+			Var = 0x50,				// 1
+			VarWithConstraint,		// 1
+			VarPack,				// 1
+			VarPackWithConstraint,	// 1
+			Const,					// 1
+			ConstWithConstraint,	// 1
+			ConstPack,				// 1
+			ConstPackWithConstraint,// 1
+			Use,					// 1
+			Init
 		};
 		enum class Pattern : unsigned
 		{
-			Var = 0x60,
-			VarWithConstraint,
-			Vars,
-			VarsWithConstraint,
-			Const,
-			ConstWithConstraint,
-			Consts,
-			ConstsWithConstraint
+			Begin = 0x60,
+			End
 		};
 		enum class Literal : unsigned
 		{
-			Int = 0x70,
-			Float,
-			String,
+			Int = 0x70,				// 1
+			Float,					// 1
+			String,					// 1
 			Null,
 			Undef,
 			True,
@@ -72,12 +67,33 @@ namespace Chtholly
 		Opcode() : value(None) {}
 
 		Opcode(Block inValue) : value(static_cast<unsigned>(inValue)) {}
-		Opcode(Lambda inValue) : value(static_cast<unsigned>(inValue)) {}
+		Opcode(Function inValue) : value(static_cast<unsigned>(inValue)) {}
 		Opcode(List inValue) : value(static_cast<unsigned>(inValue)) {}
 		Opcode(Control inValue) : value(static_cast<unsigned>(inValue)) {}
 		Opcode(Object inValue) : value(static_cast<unsigned>(inValue)) {}
 		Opcode(Pattern inValue) : value(static_cast<unsigned>(inValue)) {}
 		Opcode(Literal inValue) : value(static_cast<unsigned>(inValue)) {}
+
+		Opcode(const Opcode& opcode) = default;
+
+		Opcode& operator= (const Opcode& opcode) = default;
+
+		bool operator== (const Opcode& opcode) const
+		{
+			return unsigned{ *this } == unsigned{ opcode };
+		}
+
+		bool operator!= (const Opcode& opcode) const
+		{
+			return !(*this == opcode);
+		}
+		
+		explicit operator unsigned() const
+		{
+			return value;
+		}
+
+		~Opcode() = default;
 
 	private:
 		unsigned value;
